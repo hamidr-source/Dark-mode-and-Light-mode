400deae4ec5cc765a810518817acebc720662043
amirbahador <amirbahador.pv@gmail.com>
2023-02-24 15:21:12 +0200

    Fix bug dark mode button

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const rootElement = document.documentElement;
 const btn = document.querySelector('.dark-light-btn');
 
+const getMainColor = () => String(rootElement.style.getPropertyValue('--main-color').trim())
+
 btn.addEventListener('click', () => {
-    rootElement.style.setProperty('--bg-color', '#343434')
-    rootElement.style.setProperty('--main-color', '#bbb')
-    rootElement.style.setProperty('--border-color', '#fff')
-})
+    if (getMainColor() == '#242424') {
+      rootElement.style.setProperty('--bg-color', '#343434')
+      rootElement.style.setProperty('--main-color', '#bbb')
+      rootElement.style.setProperty('--border-color', '#fff')
+    }   
+    else{
+      rootElement.style.setProperty('--bg-color', '#ccc')
+      rootElement.style.setProperty('--main-color', '#242424')
+      rootElement.style.setProperty('--border-color', '#404949')
+    }
+
+  })
